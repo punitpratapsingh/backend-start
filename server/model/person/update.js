e1b0eb58973f9ecfa8be4185ee65d27039ad65e3
punit <punitpratapsingh30@gmail.com>
2018-12-21 14:52:00 +0530

    commit for update

--- server/model/person/update.js
+++ server/model/person/update.js
@@ -10,11 +10,42 @@ import PersonModel from '../../schemas/person';
  *
  */
 export default ({
-	userId 
-	
+	userId,
+	name,
+	phoneCode,
+	phoneNumber,
+	day,
+	month,
+	year,
+	gender,
+	nationality,
+	about,
+
 }) => new Promise((resolve, reject) => {
 	try {
-		PersonModel.findByIdAndUpdate({_id:userId},{ $set: { name: 'qwerty' }})
+		const updateObject = {};
+		if (name) { updateObject.name = name; }
+		if (phoneCode && phoneNumber) {
+			updateObject.mobile = {
+				code: phoneCode,
+				number: phoneNumber,
+			};
+		}
+		if (day && month && year) {
+			updateObject.dob = {
+				day: day,
+				month: month,
+				year: year
+			};
+		}
+
+		if (gender) { updateObject.gender = gender; }
+
+		if (nationality) { updateObject.nationality = nationality; }
+
+		if (about) { updateObject.about = about; }
+
+		PersonModel.findByIdAndUpdate({ _id: userId }, { $set: updateObject })
 			.then((success) => {
 				console.log(success);
 				resolve({ code: 100, message: 'person updated.' })
